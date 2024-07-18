1ffcff3ae8c021a4558185668a28527208245738
Alman <149833592+Almanzoris@users.noreply.github.com>
2024-07-18 04:15:45 +0200

    Ejercicio 9.

---
+++ Ejercicios/Ejercicio9/Ej9.m
@@ -0,0 +1,22 @@
+a = 0;
+b = 1;
+c = -1;
+v = -1;
+
+while 1
+    while 1
+        c = sqrt(a^2 + b^2);
+        if (a + b + c >= 1000)
+            break;
+        end
+        b = b + 1;
+    end
+    if (a + b + c == 1000) && (a < b) && (b < c)
+        v = a*b*c;
+        break;
+    elseif a == 1000
+        break;
+    end
+    a = a + 1;
+    b = a + 1;
+end
